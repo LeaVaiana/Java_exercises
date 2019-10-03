3ab2907074284961b0b0bd4c9dbe7c809a948afe
Lea Vaiana <43003349+LeaVaiana@users.noreply.github.com>
2019-10-03 17:25:55 +0200

    second_commit

--- EjercicioHerencia/EjercicioHerencia.java
+++ EjercicioHerencia/EjercicioHerencia.java
@@ -4,17 +4,24 @@ package ejercicioHerencia;
 public class EjercicioHerencia {
 
 	public static void main(String[] args) {
-		Empleado e = new Empleado("Lea", "fgfg");
-		Administracion a = new Administracion("Gigi","abcd","marketing");
+		//Creo un empleado
+		Empleado e = new Empleado(null, null);
+		Administracion a = new Administracion(null, null, null);
 		//Gerente g = new Gerente ();
 		
-		//e.setNombre("Lea");
-		//e.setDNI("ABCD");
-		//e.setSueldo(2999d);
-		//a.Administracion("obrero");
+		//Añado valor atributos al empleado
+		e.setNombre("Lea");
+		e.setDni("ABCD");
+		e.setSueldo(3001d);
+		//Añado valor atributos a administracion
+		a.setNombre("pepino");
+		a.setDni("lllll");
+		a.setSeccion("Marketing");
 		
-		System.out.println("El nombre del empleado es " + e.getNombre() + " su DNI es " + e.getDNI() + " su salario es "+ (e.getSueldo() * 0.85));
-		System.out.println("El nombre del empleado es " + e.getNombre() + " su DNI es " + e.getDNI() + " su salario es "+ (Empleado.sueldoNeto()));
+		
+		System.out.println("El nombre del empleado 1 es " + e.getNombre() + " su DNI es " + e.getDni() + " su salario es "+ (e.getSueldo() * 0.85));
+		System.out.println("El nombre del empleado 1 es " + e.getNombre() + " su DNI es " + e.getDni() + " su salario es "+ (Empleado.sueldoNeto()));
+		System.out.println("El nombre del empleado 2 es " + a.getNombre() + " su DNI es " + a.getDni() + " su departamento es "+ a.getSeccion());
 		//System.out.println("El nombre del empleado es " + e.nombre + " su DNI es " + e.DNI + ". El empleado es un " + a.seccion);
         //System.out.println("El nombre del empleado es " + e.getNombre() + " su DNI es " + e.getDNI() + " su salario es "+ (e.sueldoNeto()));
         //System.out.println("El sueldo del gerente es " + g.sueldoNeto());
